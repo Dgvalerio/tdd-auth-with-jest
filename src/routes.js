f5461cab0e725f4400d7dcbbe2a056241ecd4e1a
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-09-09 16:43:12 -0300

    :heavy_plus_sign: feat(nodemon): Add nodemon
    Adding nodemon for a better dev experience.

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,3 @@
 const routes = require('express').Router();
 
-const { User } = require('./app/models');
-
-User.create({
-  name: 'Diego',
-  email: 'diego@rocketseat.com.br',
-  password_hash: '123123123',
-});
-
 module.exports = routes;
